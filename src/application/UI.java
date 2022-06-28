bc622194490e11a1d7e8c92fd4d3fbb937acb593
Samuel Rodrigues <samucabarr169@gmail.com>
2022-06-27 21:26:44 -0300

    Pawn

--- src/application/UI.java
+++ src/application/UI.java
@@ -115,7 +115,7 @@ public class UI {
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_WHITE);
-		System.out.print("Black:");
+		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
